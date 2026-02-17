68ec1e471c55fb635007a22383e32e3e1d79efa6
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-17 16:08:03 +0000

    frameworking bottom tab

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
   }
 
   return (
-      <div className="flex flex-col items-center gap-2 text-[rgb(33,8,36)] font-[monospace] h-screen w-full">
+      <div className="flex flex-col items-center gap-2 text-[rgb(33,8,36)] font-[monospace] h-screen w-full p-2">
         <div className="flex flex-col justify-center items-center my-5">
           <h1>|Ø Songs</h1>
           <h3 className="w-fit p-0.5 text-[rgb(181,138,170)] font-[Audiowide]">
@@ -76,7 +76,7 @@ function App() {
           </h3>
         </div>
         <SearchBar onSearch={handleSearch} />
-        <div className="w-full max-w-6xl flex-1 grid grid-rows-[1fr_1fr] sm:grid-rows-none sm:grid-cols-[1fr_1fr] justify-items-stretch items-stretch gap-1 px-2 pb-2 rounded-t-2xl overflow-y-auto">
+        <div className="w-full max-w-6xl flex-1 grid grid-rows-[1fr_1fr] sm:grid-rows-none sm:grid-cols-[1fr_1fr] justify-items-stretch items-stretch gap-1  rounded-t-2xl overflow-y-auto">
           <div className="panel">
             <SearchResults tracks={searchResults} addTrack={addTrack} />
           </div>
@@ -87,6 +87,14 @@ function App() {
               removeTrack={removeTrack}
               savePlaylist={Spotify.savePlaylist.bind(Spotify)}
             />
+          </div>
+        </div>
+        <div className="inline-flex jsutify-center items-center w-full h-10 border">
+          <div className="flex-1">
+
+          </div>
+          <div className="h-full w-[30%] border">
+
           </div>
         </div>
       </div>
