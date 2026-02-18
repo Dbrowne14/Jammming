9338a9aff29ee2ff9d2d2d326e9a05d6bfd4d1e3
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-18 18:51:35 +0000

    format edits

--- src/components/Footer/Score.tsx
+++ src/components/Footer/Score.tsx
@@ -4,8 +4,8 @@ import { getScoreColor } from "../../utils/utilityFns";
 const Score = () => {
   const topScoreInSeconds = 2400000;
   const topScore = 100;
-  const weeklyTheme= "David Bowie"
-  const { totalPlaylistLength, playListTracks} = useSpotify();
+  const weeklyTheme = "David Bowie";
+  const { totalPlaylistLength, playListTracks } = useSpotify();
   const proximityToTarget = Math.floor(
     (totalPlaylistLength / topScoreInSeconds) * topScore,
   );
@@ -18,32 +18,34 @@ const Score = () => {
   };
   const finalScore = proximityConverted(proximityToTarget);
 
-  const themeCount = playListTracks.filter(track => track.trackArtist.includes(weeklyTheme)).length
+  const themeCount = playListTracks.filter((track) =>
+    track.trackArtist.includes(weeklyTheme),
+  ).length;
   const playListTrackLength = playListTracks.length;
-  const themeScore = Math.floor(themeCount/playListTrackLength * 100)
+  const themeScore = Math.floor((themeCount / playListTrackLength) * 100);
 
   return (
     <div className="inline-flex gap-2">
-        <div className="inline-flex justify-center items-center w-fit h-full  gap-1 ">
-          <h2 className="font-bold font-[Audiowide] text-[rgb(144,90,144)]">
-            Accuracy:
-          </h2>
-          <h2
-            className={`font-bold font-[Audiowide] ${getScoreColor(finalScore)} text-center rounded-2xl bg-[rgba(238,184,239,0.5)] px-2`}
-          >
-            {proximityToTarget === 0 ? "---" : finalScore}
-          </h2>
-        </div>
-            <div className="inline-flex justify-center items-center w-fit h-full  gap-1 ">
-          <h2 className="font-bold font-[Audiowide] text-[rgb(144,90,144)]">
-            Theme:
-          </h2>
-          <h2
-            className={`font-bold font-[Audiowide] ${getScoreColor(themeScore)} text-center rounded-2xl bg-[rgba(238,184,239,0.5)] px-2`}
-          >
-            {themeScore}
-          </h2>
-        </div>
+      <div className="inline-flex justify-center items-center w-fit h-full  gap-1 ">
+        <h2 className="font-bold font-[Audiowide] text-[rgb(144,90,144)]">
+          Accuracy:
+        </h2>
+        <h2
+          className={`font-bold font-[Audiowide] ${getScoreColor(finalScore)} text-center rounded-2xl bg-[rgba(238,184,239,0.5)] px-2`}
+        >
+          {proximityToTarget === 0 ? "---" : finalScore}
+        </h2>
+      </div>
+      <div className="inline-flex justify-center items-center w-fit h-full  gap-1 ">
+        <h2 className="font-bold font-[Audiowide] text-[rgb(144,90,144)]">
+          Theme:
+        </h2>
+        <h2
+          className={`font-bold font-[Audiowide] ${getScoreColor(themeScore)} text-center rounded-2xl bg-[rgba(238,184,239,0.5)] px-2`}
+        >
+          {themeScore}
+        </h2>
+      </div>
     </div>
   );
 };
