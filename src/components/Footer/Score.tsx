0b5b7257c140d1beb52c4299e3e9dbc1f716d0c9
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-18 15:44:43 +0000

    updating score

--- src/components/Footer/Score.tsx
+++ src/components/Footer/Score.tsx
@@ -1,11 +1,17 @@
 import { useSpotify } from "../../context/SpotfyContext";
-import { minutesToSeconds } from "../../utils/utilityFns";
 
 
 const Score = () => {
     const idealScore= 2400000;
     const {totalPlaylistLength} =useSpotify()
     const proximityToTarget = Math.floor(totalPlaylistLength/idealScore * 100)
+    const proximityConverted = (score:number) => {
+        if(score<=100) {
+            return score
+        } else {
+            return 100 + (100-score);
+        }
+    }
 
   return (
     <div className="inline-flex justify-center items-center w-fit h-full  gap-1 ">
@@ -13,7 +19,7 @@ const Score = () => {
         Score:
       </h2>
       <h2 className="font-bold font-[Audiowide] text-center text-[rgb(3,110,12)] rounded-2xl bg-[rgba(238,184,239,0.5)] px-2">
-        {proximityToTarget === 0 ? "--" : proximityToTarget}
+        {proximityToTarget === 0 ? "--" : proximityConverted(proximityToTarget)}
       </h2>
     </div>
   );
