66f42eac651b5952cd9e5223a6a71bbe05fa68dc
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-18 12:02:53 +0000

    adding Context

---
+++ src/context/SpotfyContext.tsx
@@ -0,0 +1,64 @@
+import { createContext, useState, useContext } from "react";
+import Spotify from "../utils/Spotify";
+import type { SpotifyTrack, Track } from "../types/types";
+import { minutesToSeconds } from "../utils/utilityFns";
+
+type SpotifyContextType = {
+  playListTracks: Track[];
+  addTrack: (track: Track) => void;
+  removeTrack: (track: Track) => void;
+  setPlaylistTracks: React.Dispatch<React.SetStateAction<Track[]>>;
+  searchResults: Track[];
+  handleSearch: (searchValue: string) => Promise<void>;
+};
+
+const SpotifyContext = createContext<SpotifyContextType | null>(null);
+
+export function useSpotify() {
+    const context = useContext(SpotifyContext);
+      if (!context) throw new Error("useSpotify must be used inside SpotifyProvider");
+  return context;
+}
+
+export function SpotifyProvider({ children }: { children: React.ReactNode }) {
+  const [playListTracks, setPlayListTracks] = useState<Track[]>([]);
+  const [searchResults, setSearchResults] = useState<Track[]>([]);
+
+  function addTrack(track: Track) {
+    if (!playListTracks.find((t) => t.id === track.id)) {
+      setPlayListTracks([...playListTracks, track]);
+    }
+  }
+
+  function removeTrack(track: Track) {
+    setPlayListTracks(playListTracks.filter((t) => t.id !== track.id));
+  }
+  async function handleSearch(searchValue: string) {
+    const results: SpotifyTrack[] = await Spotify.searchForResults(searchValue);
+    console.log(results);
+    const mappedResults: Track[] = results.map((track) => ({
+      id: track.id,
+      trackName: track.name,
+      trackArtist: track.artists.map((a) => a.name).join(", "),
+      length: minutesToSeconds(track.duration_ms),
+      uri: track.uri,
+    }));
+
+    setSearchResults(mappedResults);
+  }
+   return (
+    <SpotifyContext.Provider
+      value={{
+        playListTracks,
+        setPlayListTracks,
+        addTrack,
+        removeTrack,
+        searchResults,
+        handleSearch,
+      }}
+    >
+      {children}
+    </SpotifyContext.Provider>
+  );
+}
+}
