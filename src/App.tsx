921f5e0cc2b8ba6a84417231f45055552e6bfb98
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-18 12:47:57 +0000

    restructuring layout and adding Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
-import SearchBar from "./components/Main/SearchBar.tsx";
-import SearchResults from "./components/Main/SearchResults.tsx";
-import PlayList from "./components/Main/PlayList.tsx";
 import Spotify from "./utils/Spotify.ts";
 import BottomBar from "./components/Footer/BottomBar.tsx";
 import Header from "./components/Header/Header.tsx";
-
+import Main from "./components/Main/Main.tsx";
 
 function App() {
   //get token on render
@@ -23,18 +20,8 @@ function App() {
     <div className="flex justify-center">
       <div className="flex flex-col max-w-6xl items-center gap-2 text-[rgb(33,8,36)] font-[monospace] h-screen w-full p-2">
         <Header />
-        <main className="flex-1 w-full flex flex-col justify-center items-center">
-          <SearchBar />
-          <div className="w-full flex-1 grid grid-rows-[1fr_1fr] sm:grid-rows-none sm:grid-cols-[1fr_1fr] justify-items-stretch items-stretch gap-1  rounded-t-2xl overflow-y-auto">
-            <div className="panel rounded-t-2xl">
-              <SearchResults/>
-            </div>
-            <div className="panel rounded-t-2xl">
-              <PlayList/>
-            </div>
-          </div>
-        </main>
-        <BottomBar/>
+        <Main />
+        <BottomBar />
       </div>
     </div>
   );
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,21 @@
+import SearchBar from "./SearchBar";
+import SearchResults from "./SearchResults";
+import PlayList from "./PlayList";
+
+const Main = () => {
+  return (
+    <main className="flex-1 w-full flex flex-col justify-center items-center">
+      <SearchBar />
+      <div className="w-full flex-1 grid grid-rows-[1fr_1fr] sm:grid-rows-none sm:grid-cols-[1fr_1fr] justify-items-stretch items-stretch gap-1  rounded-t-2xl overflow-y-auto">
+        <div className="panel rounded-t-2xl">
+          <SearchResults />
+        </div>
+        <div className="panel rounded-t-2xl">
+          <PlayList />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
